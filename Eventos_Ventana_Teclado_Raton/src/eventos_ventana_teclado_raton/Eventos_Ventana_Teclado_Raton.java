89182fab18d21181807b686c57480e9d668bdb00
Jose Vidal. <User@Pepe>
2020-10-26 17:28:38 +0100

    Commit 1-- Window Finalizado.

--- Eventos_Ventana_Teclado_Raton/src/eventos_ventana_teclado_raton/Eventos_Ventana_Teclado_Raton.java
+++ Eventos_Ventana_Teclado_Raton/src/eventos_ventana_teclado_raton/Eventos_Ventana_Teclado_Raton.java
@@ -15,7 +15,7 @@ public class Eventos_Ventana_Teclado_Raton {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Vista();
     }
     
 }
